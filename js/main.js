0dbf33d40bbe71546ecafd3a27af6759703ef69b
Hass-e <131227391+Hass-e@users.noreply.github.com>
2023-09-03 18:31:32 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,632 @@
+const productosArray = [
+    //Completes
+    {
+        id: "complete-01",
+        titulo: "Complete 01",
+        imagen: "img/Completes/Complete1.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-02",
+        titulo: "Complete 02",
+        imagen: "img/Completes/Complete2.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-03",
+        titulo: "Complete 03",
+        imagen: "img/Completes/Complete3.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-04",
+        titulo: "Complete 04",
+        imagen: "img/Completes/Complete4.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-05",
+        titulo: "Complete 05",
+        imagen: "img/Completes/Complete5.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-06",
+        titulo: "Complete 06",
+        imagen: "img/Completes/Complete6.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-07",
+        titulo: "Complete 07",
+        imagen: "img/Completes/Complete7.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-08",
+        titulo: "Complete 08",
+        imagen: "img/Completes/Complete8.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-09",
+        titulo: "Complete 09",
+        imagen: "img/Completes/Complete9.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-10",
+        titulo: "Complete 10",
+        imagen: "img/Completes/Complete10.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-11",
+        titulo: "Complete 11",
+        imagen: "img/Completes/Complete11.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-12",
+        titulo: "Complete 12",
+        imagen: "img/Completes/Complete12.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-13",
+        titulo: "Complete 13",
+        imagen: "img/Completes/Complete13.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-14",
+        titulo: "Complete 14",
+        imagen: "img/Completes/Complete14.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-15",
+        titulo: "Complete 15",
+        imagen: "img/Completes/Complete15.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    {
+        id: "complete-16",
+        titulo: "Complete 16",
+        imagen: "img/Completes/Complete16.jpg",
+        categoria: {
+            nombre: "Completes",
+            id: "completes"
+        },
+        precio: 100
+    },
+    //Decks
+    {
+        id: "deck-01",
+        titulo: "Deck 01",
+        imagen: "img/Decks/Deck1.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-02",
+        titulo: "Deck 02",
+        imagen: "img/Decks/Deck2.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-03",
+        titulo: "Deck 03",
+        imagen: "img/Decks/Deck3.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-04",
+        titulo: "Deck 04",
+        imagen: "img/Decks/Deck4.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-05",
+        titulo: "Deck 05",
+        imagen: "img/Decks/Deck5.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-06",
+        titulo: "Deck 06",
+        imagen: "img/Decks/Deck6.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-07",
+        titulo: "Deck 07",
+        imagen: "img/Decks/Deck7.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-08",
+        titulo: "Deck 08",
+        imagen: "img/Decks/Deck8.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-09",
+        titulo: "Deck 09",
+        imagen: "img/Decks/Deck9.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-10",
+        titulo: "Deck 10",
+        imagen: "img/Decks/Deck10.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-11",
+        titulo: "Deck 11",
+        imagen: "img/Decks/Deck11.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-12",
+        titulo: "Deck 12",
+        imagen: "img/Decks/Deck12.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-13",
+        titulo: "Deck 13",
+        imagen: "img/Decks/Deck13.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-14",
+        titulo: "Deck 14",
+        imagen: "img/Decks/Deck14.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-15",
+        titulo: "Deck 15",
+        imagen: "img/Decks/Deck15.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    {
+        id: "deck-16",
+        titulo: "Deck 16",
+        imagen: "img/Decks/Deck16.png",
+        categoria: {
+            nombre: "Decks",
+            id: "decks"
+        },
+        precio: 80
+    },
+    //Trucks
+    {
+        id: "truck-01",
+        titulo: "Trcuk 01",
+        imagen: "img/Trucks/Trucks1.png",
+        categoria: {
+            nombre: "Trucks",
+            id: "trucks"
+        },
+        precio: 50
+    },
+    {
+        id: "truck-02",
+        titulo: "Trcuk 02",
+        imagen: "img/Trucks/Trucks2.png",
+        categoria: {
+            nombre: "Trucks",
+            id: "trucks"
+        },
+        precio: 50
+    },
+    {
+        id: "truck-03",
+        titulo: "Trcuk 03",
+        imagen: "img/Trucks/Trucks3.png",
+        categoria: {
+            nombre: "Trucks",
+            id: "trucks"
+        },
+        precio: 50
+    },
+    {
+        id: "truck-04",
+        titulo: "Trcuk 04",
+        imagen: "img/Trucks/Trucks4.png",
+        categoria: {
+            nombre: "Trucks",
+            id: "trucks"
+        },
+        precio: 50
+    },
+    {
+        id: "truck-05",
+        titulo: "Trcuk 05",
+        imagen: "img/Trucks/Trucks5.png",
+        categoria: {
+            nombre: "Trucks",
+            id: "trucks"
+        },
+        precio: 50
+    },
+    {
+        id: "truck-06",
+        titulo: "Trcuk 06",
+        imagen: "img/Trucks/Trucks6.png",
+        categoria: {
+            nombre: "Trucks",
+            id: "trucks"
+        },
+        precio: 50
+    },
+    {
+        id: "truck-07",
+        titulo: "Trcuk 07",
+        imagen: "img/Trucks/Trucks7.png",
+        categoria: {
+            nombre: "Trucks",
+            id: "trucks"
+        },
+        precio: 50
+    },
+    {
+        id: "truck-08",
+        titulo: "Trcuk 8",
+        imagen: "img/Trucks/Trucks8.png",
+        categoria: {
+            nombre: "Trucks",
+            id: "trucks"
+        },
+        precio: 50
+    },
+    //Wheels
+    {
+        id: "wheels-01",
+        titulo: "Wheels 01",
+        imagen: "img/Wheels/Wheels1.png",
+        categoria: {
+            nombre: "Wheels",
+            id: "wheels"
+        },
+        precio: 50
+    },
+    {
+        id: "wheels-02",
+        titulo: "Wheels 02",
+        imagen: "img/Wheels/Wheels2.png",
+        categoria: {
+            nombre: "Wheels",
+            id: "wheels"
+        },
+        precio: 50
+    },
+    {
+        id: "wheels-03",
+        titulo: "Wheels 03",
+        imagen: "img/Wheels/Wheels3.png",
+        categoria: {
+            nombre: "Wheels",
+            id: "wheels"
+        },
+        precio: 50
+    },
+    {
+        id: "wheels-04",
+        titulo: "Wheels 04",
+        imagen: "img/Wheels/Wheels4.png",
+        categoria: {
+            nombre: "Wheels",
+            id: "wheels"
+        },
+        precio: 50
+    },
+    {
+        id: "wheels-05",
+        titulo: "Wheels 05",
+        imagen: "img/Wheels/Wheels5.png",
+        categoria: {
+            nombre: "Wheels",
+            id: "wheels"
+        },
+        precio: 50
+    },
+    {
+        id: "wheels-06",
+        titulo: "Wheels 06",
+        imagen: "img/Wheels/Wheels6.png",
+        categoria: {
+            nombre: "Wheels",
+            id: "wheels"
+        },
+        precio: 50
+    },
+    {
+        id: "wheels-07",
+        titulo: "Wheels 07",
+        imagen: "img/Wheels/Wheels7.png",
+        categoria: {
+            nombre: "Wheels",
+            id: "wheels"
+        },
+        precio: 50
+    },
+    {
+        id: "wheels-08",
+        titulo: "Wheels 08",
+        imagen: "img/Wheels/Wheels8.png",
+        categoria: {
+            nombre: "Wheels",
+            id: "wheels"
+        },
+        precio: 50
+    },
+    {
+        id: "wheels-09",
+        titulo: "Wheels 09",
+        imagen: "img/Wheels/Wheels9.png",
+        categoria: {
+            nombre: "Wheels",
+            id: "wheels"
+        },
+        precio: 50
+    },
+    {
+        id: "wheels-10",
+        titulo: "Wheels 10",
+        imagen: "img/Wheels/Wheels10.png",
+        categoria: {
+            nombre: "Wheels",
+            id: "wheels"
+        },
+        precio: 50
+    },
+    {
+        id: "wheels-11",
+        titulo: "Wheels 11",
+        imagen: "img/Wheels/Wheels11.png",
+        categoria: {
+            nombre: "Wheels",
+            id: "wheels"
+        },
+        precio: 50
+    },
+    {
+        id: "wheels-12",
+        titulo: "Wheels 12",
+        imagen: "img/Wheels/Wheels12.png",
+        categoria: {
+            nombre: "Wheels",
+            id: "wheels"
+        },
+        precio: 50
+    }
+]
+
+
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+    
+    productosElegidos.forEach(producto => {
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                <div class="producto-detalles">
+                    <h3 class="producto-titulo">${producto.titulo}</h3>
+                    <p class="producto-precio">${producto.precio}</p>
+                    <button class="producto-agregar" id="${producto.id}">Add</button>
+                </div>
+        `;
+
+        contenedorProductos.append(div);
+    })
+
+    actualizarBotonesAgregar();
+}
+
+
+
+const productosCompletes = productosArray.filter(producto => producto.categoria.id === "completes")
+cargarProductos(productosCompletes);                                                                              //Se agregan automaticamente los elementos a HTML
+
+
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if(e.currentTarget.id != "all") {
+            const productoCategoria = productosArray.find( producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productosArray.filter(producto => producto.categoria.id === e.currentTarget.id)
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "All products"
+            cargarProductos(productosArray);
+        }
+
+    })
+});
+
+
+
+function actualizarBotonesAgregar(){
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCaarrito);
+    });
+}
+
+
+
+let productosEnCarrito;
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if(productosEnCarritoLS){
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+
+
+function agregarAlCaarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productosArray.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)){
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else{
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+}
+
+
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
